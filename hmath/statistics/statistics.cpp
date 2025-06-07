abf0d7c79fbf7eb35b490c7cb1c9e18e95a241f7
Hveppe <mikkelhvelp@gmail.com>
2025-06-07 14:11:26 +0200

    Add statistical functions for integer input: average, median, and variance

--- hmath/statistics/statistics.cpp
+++ hmath/statistics/statistics.cpp
@@ -1,6 +1,7 @@
 #include "statistics.h"
 
 namespace hmath {
+    // double input
     double average(std::vector<double> numbers) {
         double result = 0;
 
@@ -26,6 +27,39 @@ namespace hmath {
         double average = hmath::average(numbers);
 
         for(double number : numbers) {
+            double delta = number - average;
+            result += delta * delta;
+        }
+
+        return result / (numbers.size() - 1);
+    }
+
+    // int input
+    double average(std::vector<int> numbers) {
+        double result = 0;
+
+        for(int i : numbers) {
+            result += i;
+        }
+
+        return result / numbers.size();
+    }
+
+    double median(std::vector<int> numbers) {
+        std::sort(numbers.begin(), numbers.end());
+        size_t size = numbers.size();
+        
+        if(size % 2 == 1) {
+            return numbers[size/2];
+        }
+        return (numbers[size / 2 - 1] + numbers[size / 2]) / 2.0;
+    }
+
+    double variance(std::vector<int> numbers) {
+        double result = 0.0;
+        double average = hmath::average(numbers);
+
+        for(int number : numbers) {
             double delta = number - average;
             result += delta * delta;
         }
--- hmath/statistics/statistics.h
+++ hmath/statistics/statistics.h
@@ -7,9 +7,15 @@
 #include "../algebra/basic_math/basic_math.h"
 
 namespace hmath {
+    // double input
     double average(std::vector<double> numbers);
     double median(std::vector<double> numbers);
     double variance(std::vector<double> numbers);
+
+    // int input
+    double average(std::vector<int> numbers);
+    double median(std::vector<int> numbers);
+    double variance(std::vector<int> numbers);
 }
 
 #endif
