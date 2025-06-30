6ed376c63f916e67ffadb9b95fc79b7df2e2b5fa
Hveppe <mikkelhvelp@gmail.com>
2025-06-30 20:49:26 +0200

    Add additional mathematical constants to the constants header

--- hmath/algebra/constants.h
+++ hmath/algebra/constants.h
@@ -2,12 +2,18 @@
 #define HMATH_CONSTANTS_H
 
 #include <limits>
+#include "roots/roots.h"
 
 namespace hmath {
     constexpr double PI = 3.14159265358979323846;
     constexpr double EULER = 2.71828182845904523536;
+    const double OMEGA = 0.56714329040978387300;
+    const double GELFONDCONSTANT = hmath::exp(hmath::PI);
+    const double GELFONDSCHNEIDERCONSTANT = hmath::logPow(2, hmath::sqrt(2));
     constexpr double TAU = 2 * hmath::PI;
     constexpr double INF = std::numeric_limits<double>::infinity();
+    constexpr double RECIPROCALFIBONACCICONSTANT = 3.359885666243177553172011;
+    const double GOLDENRATIO = (1 + hmath::sqrt(5))/2;
 }
 
 #endif
